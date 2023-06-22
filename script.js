5006591e32b98feb4096b72b4c29439cbf85e3da
OoriFogel <ufogelu@gmail.com>
2023-06-22 14:23:53 +0300

    vertical

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const $ = (selector,parent = document) => parent.querySelector(selector);
 const $$ = (selector,parent = document) => parent.querySelectorAll(selector);
 
 const first_menu = $$('.menu > .menu-item');
+const sub_menu = $$('.menu > .menu-item > .submenu-container > span.menu-item');
 
 function addChevron(item, src) {
     item.forEach(element => {
@@ -12,13 +13,13 @@ function addChevron(item, src) {
         element.insertBefore(chevron, element.childNodes[3]);
       }
     });
-}
+};
   
 function subChevron(item, src){
   item.forEach(node => {
       addChevron($$('.submenu-container > .menu-item',node),src);
   });
-}
+};
 
 function subToggle(){
     $$('.chevron').forEach(img => {
@@ -30,12 +31,20 @@ function subToggle(){
     $('section').onclick = function() {
         first_menu.forEach(element => { 
                 element.classList.remove('active');
-        })
-    }
-}
+        });
+        sub_menu.forEach(element => { 
+            element.classList.remove('active');
+        });
+    };
+};
 
-
-addChevron(first_menu,'img/chevron-light.svg');
-subChevron(first_menu,'img/chevron.svg');
-subToggle();
-
+function chevronSet(){
+    if($('.menu').parentNode.classList.contains('horizontal')){
+        addChevron(first_menu,'img/chevron-light.svg');
+        subChevron(first_menu,'img/chevron.svg');
+    } else if($('.menu').parentNode.classList.contains('vertical')){
+        subChevron(first_menu,'img/chevron-light-side.svg');
+    };
+    subToggle();
+};
+chevronSet();
